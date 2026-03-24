7480dff1bfc7fe4df977ea3076d019ea31f032b2
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2026-03-24 22:22:30 +0700

    s

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,23 +1,23 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { IUser } from './user.interface';
- 
+
 import { readFileSync } from 'fs';
 import { join } from 'path';
- 
+
 const filePath = join(__dirname, '../../data/users.json');
- 
+
 @Injectable()
 export class UserService {
   test() {
     return [];
   }
- 
+
   findAll(): IUser[] {
     const readUserData = readFileSync(filePath, 'utf-8');
     const userData = JSON.parse(readUserData) as IUser[];
     return userData;
   }
- 
+
   findOne(id: string, fields?: string[]): Partial<IUser> {
     const users = this.findAll();
     const user = users.find((u) => u.id === id);
@@ -38,4 +38,3 @@ export class UserService {
     }, {} as Partial<IUser>);
   }
 }
- 
